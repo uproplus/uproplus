c94468cbf8a505b7115f60a91670698f0142e162
uproplus <63788079+uproplus@users.noreply.github.com>
2020-04-18 15:23:00 +0900

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uproplus/core/services/user_service.dart';
+import 'package:uproplus/locator.dart';
+import 'package:uproplus/ui/router.dart';
+import 'package:uproplus/ui/shared/app_colors.dart';
+
+Future<bool> _isLoggedIn() async {
+  return await locator<UserService>().init();
+}
+
+Future _runAppAsync() async {
+  var initialRoute = await _isLoggedIn() ? '/' : 'login';
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+  runApp(MyApp(initialRoute));
+}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
+  _runAppAsync();
+}
+
+class MyApp extends StatelessWidget {
+  final String initialRoute;
+
+  MyApp(this.initialRoute);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          scaffoldBackgroundColor: Colors.white,
+          accentColor: Color(0xffa67c00),
+          primarySwatch: materialGoldColor),
+      initialRoute: initialRoute,
+      onGenerateRoute: Router.generateRoute,
+    );
+  }
+}
