1820c9998045a143d3bdfd5c08c290c9bd72873b
uproplus <63788079+uproplus@users.noreply.github.com>
2020-04-18 16:55:18 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:uproplus/core/services/user_service.dart';
-import 'package:uproplus/locator.dart';
 import 'package:uproplus/ui/router.dart';
 import 'package:uproplus/ui/shared/app_colors.dart';
 
-void main() => runApp(MyApp(initialRoute));
+void main() => runApp(MyApp());
 
-Future<bool> _isLoggedIn() async {
-  return await locator<UserService>().init();
-}
 
-Future _runAppAsync() async {
-  var initialRoute = await _isLoggedIn() ? '/' : 'login';
-  await SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
-  runApp(MyApp(initialRoute));
-}
+
+
 
 class MyApp extends StatelessWidget {
-  final String initialRoute;
 
-  MyApp(this.initialRoute);
 
   @override
   Widget build(BuildContext context) {
